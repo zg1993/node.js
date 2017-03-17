d3bd531568e28759b7c797e0470be5a1557e9bc2
zg1993 <13122780440@163.com>
2017-03-17 18:58:16 +0800

    note

---
+++ note/test.py
@@ -0,0 +1,15 @@
+#_*_ coding:utf-8 _*_
+
+
+import os 
+import urllib
+import chardet
+
+a=''
+#chardet.detect(a)
+#fs = open('C:\Users\kyz\Desktop\documentation.txt', 'r')
+fs = open('1.txt')
+f = fs.read()
+#chardet.detect(f)
+
+# print(type(f))
